b5e5cb8a9ebdb5499db6e81fb0633a0a7befefaa
KevinKiely <Kevinkiely762@gmail.com>
2024-03-14 17:06:07 -0400

    Finished adding/removing styling for text boxes based on current hour, properly linked JS and style.css files, finishing touches

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 $(document).ready(function () {
 
-
-  $('#block-10').children().eq(1).val(localStorage.getItem("tenAM"));
   $('#block-9').children().eq(1).val(localStorage.getItem("nineAM"));
+  $('#block-10').children().eq(1).val(localStorage.getItem("tenAM"));
+  $('#block-11').children().eq(1).val(localStorage.getItem("elevenAM"));
+  $('#block-12').children().eq(1).val(localStorage.getItem("twelvePM"));
+  $('#block-13').children().eq(1).val(localStorage.getItem("onePM"));
+  $('#block-14').children().eq(1).val(localStorage.getItem("twoPM"));
+  $('#block-15').children().eq(1).val(localStorage.getItem("threePM"));
+  $('#block-16').children().eq(1).val(localStorage.getItem("fourPM"));
+  $('#block-17').children().eq(1).val(localStorage.getItem("fivePM"));
+  
 
   $('.saveBtn').on('click', function () {
-    console.log("entered saved text");
     var nineAM = $('#block-9').children().eq(1).val();
-    //GRAB VALUE FOR EACH TIME BLOCK
-
     localStorage.setItem("nineAM", nineAM);
 
     var tenAM = $('#block-10').children().eq(1).val();
     localStorage.setItem("tenAM", tenAM);
+
+    var elevenAM = $('#block-11').children().eq(1).val();
+    localStorage.setItem("elevenAM", elevenAM);
+
+    var twelvePM = $('#block-12').children().eq(1).val();
+    localStorage.setItem("twelvePM", twelvePM);
+
+    var onePM = $('#block-13').children().eq(1).val();
+    localStorage.setItem("onePM", onePM);
+
+    var twoPM = $('#block-14').children().eq(1).val();
+    localStorage.setItem("twoPM", twoPM);
+
+    var threePM = $('#block-15').children().eq(1).val();
+    localStorage.setItem("threePM", threePM);
+
+    var fourPM = $('#block-16').children().eq(1).val();
+    localStorage.setItem("fourPM", fourPM);
+
+    var fivePM = $('#block-17').children().eq(1).val();
+    localStorage.setItem("fivePM", fivePM);
   });
 
   var hour= parseInt(dayjs().format('HH'));
@@ -31,7 +53,6 @@ $(document).ready(function () {
     if (currentTime === hour) {
       console.log('here');
       $(this).addClass('present');
-      console.log($(this));
       $(this).removeClass('past');
       $(this).removeClass('future');
     }
@@ -48,42 +69,7 @@ $(document).ready(function () {
     }
   });
 
-
   var now = dayjs().format('MMM DD, YYYY');
   $('#currentDay').text(now);
 
-
-
 });
-
-
-
-
-
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-// Use currentHour= dayjs().format to get the hour, then compare the current hour to the value of the ID hour. Use an if/else if/else statement. If the hour=IDhour, add attribute "current" to the attribute of the block. If
-
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
-
-
-
